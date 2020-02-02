be1a6d5129813b8e708b51a3b1ea25185719bd99
Max Krak <krakenslab@gmail.com>
2020-02-02 19:42:20 +0300

    lesson 10

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Technologies from './components/Technologies'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Technologies />
       Simple HTML
     </div>
   );
-}
-
-const Header = () => {
-  return (
-    <div>
-      <a href='#'>Home</a>
-      <a href='#'>News Feed</a>
-      <a href='#'>Messages</a>
-    </div>
-  )
 }
 
 export default App;
