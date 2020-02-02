5e90f77c671ed68fecc94b6cb99ad7837f5d798a
Max Krak <krakenslab@gmail.com>
2020-02-02 19:53:32 +0300

    lesson 10

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header'
 import Technologies from './components/Technologies'
+import Header from './components/Header';
 
 const App = () => {
   return (
