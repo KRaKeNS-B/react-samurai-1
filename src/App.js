c15b839eaeca6c3fa64a314f2008b0c66af98f67
Max Krak <krakenslab@gmail.com>
2020-02-03 21:36:25 +0300

    lesson 12

--- src/App.js
+++ src/App.js
@@ -1,53 +1,16 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src="https://s1.logaster.com/static/v3/img/products/logo.png" alt="Лого"/>
-      </header>
-      <nav className='nav'>
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src="https://openimagedenoise.github.io/images/moana_16spp_oidn.jpg" alt=""/>
-        </div>
-        <div>
-          <img src="https://static-cdn.123rf.com/images/v5/index-thumbnail/84170952-b.jpg" alt=""/>
-          description
-        </div>
-        <div>
-          My posts
-          <div>
-            New post
-          </div>
-          <div>
-            <div>
-              post 1
-            </div>
-            <div>
-              post 2
-            </div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://openimagedenoise.github.io/images/moana_16spp_oidn.jpg" alt=""/>
+      </div>
+      <div>
+        <img src="https://static-cdn.123rf.com/images/v5/index-thumbnail/84170952-b.jpg" alt=""/>
+        description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
