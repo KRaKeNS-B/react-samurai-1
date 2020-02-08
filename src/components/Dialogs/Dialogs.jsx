9d623972ff4411f06df74752ccf689bf113d5d17
Max Krak <krakenslab@gmail.com>
2020-02-08 18:27:52 +0300

    lesson 24 data

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,6 +19,20 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+  let dialogsData = [
+    { id: '1', name: 'Dimych' },
+    { id: '2', name: 'Andrey' },
+    { id: '3', name: 'Mary' },
+    { id: '4', name: 'Sveta' },
+  ]
+  let messagesData = [
+    { id: '1', message: 'Hi'},
+    { id: '2', message: 'How are you?'},
+    { id: '3', message: 'Yo'},
+    { id: '4', message: 'Yo'},
+    { id: '5', message: 'Yo'},
+  ]
+
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,11 @@ import classes from './MyPosts.module.css'
 import Post from './Post/Post';
 
 const MyPosts = () => {
+  let postsData = [
+    { id: '1', message: `Hi, how are you?`, likesCount: '11'},
+    { id: '1', message: `It's my first post`, likesCount: '23'},
+  ]
+
   return (
     <div className={classes.postsBlock} >
       <h3>My posts</h3>
