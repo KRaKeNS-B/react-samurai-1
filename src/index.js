b1e4f0df2c055190da18ce2ca5b19c2014e5849f
Max Krak <krakenslab@gmail.com>
2020-02-12 21:13:56 +0300

    lesson 37 store, bind

--- src/index.js
+++ src/index.js
@@ -4,24 +4,24 @@ import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 import './index.css'
 import App from './App'
-import state, { addPost, updateNewPostText, subscribe } from './redux/state'
+import store from './redux/state'
 
-let rerenderEntireTree = () => {
+let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
       <App
         state={ state }
-        addPost={ addPost }
-        updateNewPostText={ updateNewPostText }
+        addPost={ store.addPost.bind(store) }
+        updateNewPostText={ store.updateNewPostText.bind(store) }
       />
     </BrowserRouter>,
     document.getElementById('root')
   )
 }
 
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
