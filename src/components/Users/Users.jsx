7e3e15499fa17e266c39d9f14385dd272a48cf56
Max Krak <krakenslab@gmail.com>
2020-02-24 22:42:47 +0300

    lesson 51 api added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import styles from './Users.module.css'
+import * as axios from 'axios'
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
 
   if (props.users.length === 0) {
-    props.setUsers([
-      { id: 1, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: false, fullName: 'Dimych', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
-      { id: 2, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: true, fullName: 'Andrey', status: 'I am a boss 2', location: { city: 'Moscow', country: 'Russia' } },
-      { id: 3, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: false, fullName: 'Mary', status: 'I am a boss too', location: { city: 'Kiev', country: 'Ukraine' } },
-      { id: 4, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: true, fullName: 'Sveta', status: 'I am a boss 3', location: { city: 'Moscow', country: 'USA' } },
-    ])
+
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        props.setUsers(response.data.items)
+      })
+
   }
-  
+
   let usersElements = props.users.map( user =>
     <div key={ user.id }>
       <span>
         <div>
-          <img src={ user.avatarUrl } alt="Ava" className={ styles.userPhoto } />
+          <img src={ user.photos.small != null ? user.photos.small : userPhoto } alt="Ava" className={ styles.userPhoto } />
         </div>
         <div>
           { user.followed
@@ -28,7 +30,7 @@ let Users = (props) => {
       <span>
         <span>
           <div>
-            { user.fullName }
+            { user.name }
           </div>
           <div>
             { user.status }
@@ -36,10 +38,10 @@ let Users = (props) => {
         </span>
         <span>
           <div>
-            { user.location.city }
+            { "user.location.city" }
           </div>
           <div>
-            { user.location.country }
+            { "user.location.country" }
           </div>
         </span>
       </span>
--- src/redux/usersReduser.js
+++ src/redux/usersReduser.js
@@ -4,11 +4,11 @@ const SET_USERS = 'SET_USERS'
 
 let initialState = {
   users: [
-    { id: 1, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: false, fullName: 'Dimych', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
-    { id: 2, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: true, fullName: 'Andrey', status: 'I am a boss 2', location: { city: 'Moscow', country: 'Russia' } },
-    { id: 3, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: false, fullName: 'Mary', status: 'I am a boss too', location: { city: 'Kiev', country: 'Ukraine' } },
-    { id: 4, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: true, fullName: 'Sveta', status: 'I am a boss 3', location: { city: 'Moscow', country: 'USA' } },
-    { id: 5, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: false, fullName: 'Vova', status: 'I am a boss 5', location: { city: 'Kiev', country: 'Germany' } },
+    // { id: 1, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: false, fullName: 'Dimych', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
+    // { id: 2, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: true, fullName: 'Andrey', status: 'I am a boss 2', location: { city: 'Moscow', country: 'Russia' } },
+    // { id: 3, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: false, fullName: 'Mary', status: 'I am a boss too', location: { city: 'Kiev', country: 'Ukraine' } },
+    // { id: 4, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: true, fullName: 'Sveta', status: 'I am a boss 3', location: { city: 'Moscow', country: 'USA' } },
+    // { id: 5, avatarUrl: 'https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg', followed: false, fullName: 'Vova', status: 'I am a boss 5', location: { city: 'Kiev', country: 'Germany' } },
   ],
 }
 
