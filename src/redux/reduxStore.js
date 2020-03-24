776efc73911b602b020baf4af13568ccaaeaade6
Max Krak <krakenslab@gmail.com>
2020-03-25 00:31:05 +0300

    lesson 61 login by cookie

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -3,14 +3,18 @@ import profileReducer from "./profileReduser";
 import dialogsReducer from "./dialogsReduser";
 import sidebarReducer from "./sidebarReducer";
 import usersReducer from "./usersReduser";
+import authReducer from "./authReduser";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarReducer,
   usersPage: usersReducer,
+  auth: authReducer,
 })
 
 let store = createStore(reducers)
+
+window.store = store
 
 export default store
